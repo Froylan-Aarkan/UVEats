419052b2902103700280d955ad554580f1fb9f2f
Froylan <froylan.16.super@outlook.com>
2024-11-07 23:41:50 -0600

    Adición de funciones
    Se añadio un carrito de compras, mapa de google maps para elegir ubicación y se mejoró el diseño de los formularios de registro e inicio de sesión

--- js/script.js
+++ js/script.js
@@ -38,6 +38,18 @@ window.addEventListener('click', (event) => {
   }
 });
 
+//switch links
+document.getElementById('showSignup').addEventListener('click', (e) => {
+  e.preventDefault();
+  hideModal('loginForm');
+  showModal('signupForm');
+});
+  
+document.getElementById('showLogin').addEventListener('click', (e) => {
+  e.preventDefault();
+  hideModal('signupForm');
+  showModal('loginForm');
+});
 
 //Menu
 const menuToggle = document.querySelector('.menu-toggle');
@@ -52,4 +64,53 @@ menuToggle.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     menu.classList.remove('active');
     overlay.classList.remove('active');
+});
+
+//cart
+const cartLink = document.querySelector('.cart-link');
+cartLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    // Código para abrir el carrito o modal con los productos. //pendiente
+    alert("Carrito de compras");
+});
+
+//Map
+let map, marker;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 }, // Ubicación inicial
+        zoom: 8,
+    });
+    marker = new google.maps.Marker({
+        position: { lat: -34.397, lng: 150.644 },
+        map: map,
+        draggable: true,
+        title: "Arrastra para seleccionar la ubicación",
+    });
+
+    google.maps.event.addListener(marker, 'dragend', function() {
+        const position = marker.getPosition();
+        console.log("Ubicación seleccionada:", position.lat(), position.lng());
+    });
+}
+
+document.getElementById("locationLink").addEventListener("click", (event) => {
+  event.preventDefault();
+  document.getElementById("mapModal").style.display = "flex";
+});
+
+document.getElementById("confirmLocation").addEventListener("click", () => {
+  const position = marker.getPosition();
+  const lat = position.lat().toFixed(5);
+  const lng = position.lng().toFixed(5);
+  document.getElementById("locationLabel").textContent = `Lat: ${lat}, Lng: ${lng}`;
+  document.getElementById("mapModal").style.display = "none";
+});
+// Cerrar modal cuando se hace clic fuera del contenido
+window.addEventListener("click", (event) => {
+    const mapModal = document.getElementById("mapModal");
+    if (event.target === mapModal) {
+        mapModal.style.display = "none";
+    }
 });
