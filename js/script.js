49af2f8bf39960aa077bd2f2b8b1f35601d39cb8
Froylan <froylan.16.super@outlook.com>
2024-12-12 13:43:19 -0600

    Adición de pagina para restaurante

--- js/script.js
+++ js/script.js
@@ -51,21 +51,6 @@ document.getElementById('showLogin').addEventListener('click', (e) => {
   showModal('loginForm');
 });
 
-//Menu
-const menuToggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
-const overlay = document.querySelector('.overlay');
-
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    overlay.classList.toggle('active');
-});
-
-overlay.addEventListener('click', () => {
-    menu.classList.remove('active');
-    overlay.classList.remove('active');
-});
-
 //Map
 let map, marker;
 
